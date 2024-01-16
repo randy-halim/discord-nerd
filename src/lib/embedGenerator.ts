1149de6cf3a6c2bab3867058c994e1fe5ef02bf5
randy-halim <randy.halim@outlook.com>
2024-01-15 17:24:54 -0800

    minor changes to base embed

--- src/lib/embedGenerator.ts
+++ src/lib/embedGenerator.ts
@@ -1,32 +1,20 @@
-import {Colors, EmbedBuilder} from "discord.js";
-import {version} from "../../package.json";
+import { Colors, EmbedBuilder } from "discord.js";
+import { version } from "../../package.json";
 
-const createBaseEmbed = () => new EmbedBuilder()
-    .setAuthor({
-        name: "AI Conversation Summarizer (github.com)",
-        url: "https://github.com/randy-halim/discord-ai-conversation-summary"
-    })
-    .setFooter({
-        text: `Built by randy <3 - version ${version}`,
-    })
+const createBaseEmbed = () =>
+  new EmbedBuilder().setAuthor({
+    name: `AI Conversation Summarizer (v${version})`,
+    url: "https://github.com/randy-halim/discord-ai-conversation-summary",
+  });
 
 export const createErrorEmbed = (msg = "An unknown error occured.") => {
-    return createBaseEmbed()
-        .setColor(Colors.Red)
-        .setDescription(msg)
-        .toJSON();
-}
+  return createBaseEmbed().setColor(Colors.Red).setDescription(msg).toJSON();
+};
 
 export const createWarnEmbed = (msg: string) => {
-    return createBaseEmbed()
-        .setColor(Colors.Yellow)
-        .setDescription(msg)
-        .toJSON()
-}
+  return createBaseEmbed().setColor(Colors.Yellow).setDescription(msg).toJSON();
+};
 
 export const createInfoEmbed = (msg: string) => {
-    return createBaseEmbed()
-        .setColor(Colors.Aqua)
-        .setDescription(msg)
-        .toJSON()
-}
+  return createBaseEmbed().setColor(Colors.Aqua).setDescription(msg).toJSON();
+};
